91d0238ad053a40f897de18a853012981288dd3d
muskan <muskan8878h@gmail.com>
2024-07-30 17:57:07 +0530

    .

---
+++ javascript/result.js
@@ -0,0 +1,13 @@
+function Addition(m1,m2,m3,m4){
+    let marks1=m1;
+    let marks2=m2;
+    let marks3=m3;
+    let marks4=m4;
+    let total=marks1+marks2+marks3+marks4;
+    return total;
+}
+
+// function CGPA(m1,m2,m3,m4){
+//     let cgp = ((marks1+marks2+marks3+marks4)/400)*10;
+//     return cgp;
+// }
