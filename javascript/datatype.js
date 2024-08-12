411f074afe7eb58c31231d6cb698ed3b06c78aa6
muskan <muskan8878h@gmail.com>
2024-08-12 17:57:41 +0530

    learn about datatype in javascript and javascript operator

---
+++ javascript/datatype.js
@@ -0,0 +1,16 @@
+let mydata; 
+console.log(typeof(mydata)); 
+mydata=null; 
+consl=ole.log(typeof(mydata)); 
+mydata=""; 
+console.log(typeof(mydata)); 
+mydata=10; 
+console.log(typeof(mydata)); 
+mydata=10.50; 
+console.log(typeof(mydata)); 
+mydata=98797889788968997n; 
+console.log(typeof(mydata)); 
+mydata=true; 
+console.log(typeof(mydata)); 
+mydata=[1,2,3,4]; 
+console.log(typeof(mydata));
